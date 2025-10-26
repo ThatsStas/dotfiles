189b3e657fa6a6c6fe5e0a24069c099d4f513757
ThatsStas <stanislav.uschakow@gmail.com>
2025-10-26 22:03:21 +0100

    Fixing p10k installation by checking for the theme folder
    instead of the config file.

--- run_once_install_p10k.sh
+++ run_once_install_p10k.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -f "$HOME/.p10k.zsh" ]]; then                                                                                                                      
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]]; then                                                                                                                      
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"                      
 else
     echo "p10k already install. Skipping"
